f55898b848e4688cda773e7ce52e8abeb134415f
SHERIF MOHAMED <Sherif425@gmail.com>
2026-08-05 10:55:59 +0300

    updating cpu and memory modules using psutil

--- projects/python/inventory-collector/inventory/cpu.py
+++ projects/python/inventory-collector/inventory/cpu.py
@@ -1,7 +1,29 @@
-import os
+# import os
 
-def get_cpu_info():
-    return {
+# def get_cpu_info():
+#     return {
         
-        "logical_cores": os.cpu_count() 
+#         "logical_cores": os.cpu_count() 
+#     }
+
+import psutil
+from typing import Any
+
+
+def get_cpu_info() -> dict[str, Any]:
+    """
+    Collect CPU Info
+    """
+    freq = psutil.cpu_freq()
+
+    return {
+        "physical_cores": psutil.cpu_count(logical=False),
+        "logical_cores": psutil.cpu_count(logical=True),
+        "usage_percent": psutil.cpu_percent(interval=1),
+        "frequency": {
+            "current_mhz": round(freq.current, 2) if freq else None,
+            "min_hhz": round(freq.min, 2) if freq else None,
+            "max_freq": round(freq.max, 2) if freq else None,
+        },
+
     }
--- projects/python/inventory-collector/inventory/memory.py
+++ projects/python/inventory-collector/inventory/memory.py
@@ -1,21 +1,47 @@
-import platform
+# import platform
+# from typing import Any
+
+# def get_memory_info() -> dict["str", Any]:
+#     """
+#     Collect Memory Information.
+
+#     Returns:
+#         Dictionary containing memory details.
+#     """
+
+
+#     return {
+#         "status": "Available in Sprint 02",
+#         "platform": platform.system(),
+#         "note": (
+#             "Detailed memory statistics require platform-specific "
+#             "APIs or the psutil package."
+#         ),
+
+#     }
+
+# memory module using psutil
+import psutil
 from typing import Any
 
-def get_memory_info() -> dict["str", Any]:
+def get_memory_info() -> dict[str, Any]:
     """
-    Collect Memory Information.
-
-    Returns:
-        Dictionary containing memory details.
+    Collect Memory information
     """
 
+    vm = psutil.virtual_memory()
+    swap = psutil.swap_memory()
 
     return {
-        "status": "Available in Sprint 02",
-        "platform": platform.system(),
-        "note": (
-            "Detailed memory statistics require platform-specific "
-            "APIs or the psutil package."
-        ),
-
+        "virtual":{
+            "total_gb": round(vm.total / (1024**3), 2),
+            "available_gb": round(vm.available / (1024 **3), 2),
+            "used_gb": round(vm.used /(1024 ** 3), 2),
+            "usage_percent": vm.percent,
+        },
+        "swap":{
+            "total_gb": round(swap.total / (1024**3), 2),
+            "used_gb": round(swap.used / (1024**3), 2),
+            "usage_percent": swap.percent,
+        },
     }
